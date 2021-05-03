0e138a592807ba90473a4970f1fa7407a1370e9c
Philippe Keita <philippekeita@Philippes-MacBook-Air-4.local>
2021-05-02 21:12:01 -0400

    tried to add some attacks in Fire

--- src/Fire.java
+++ src/Fire.java
@@ -1,9 +1,84 @@
 
 public class Fire extends Pokemon {
 
+	private final String TYPE = "fire";
+
+	public Fire(String name){
+		super(name);
+	}
+
 	// attack for fire pokemon
 	@Override
 	public void attack() {
+
+	}
+
+
+
+	public int storm(Pokemon opponent, String def){
+		int damage = 0;
+
+		if (def.equals("shell")){
+			damage += 15;
+		}
+
+		if (def.equals("shield")){
+			damage  += 25;
+		}
+
+		if (def.equals("simple")){
+			damage  += 45;
+		}
+
+		if(opponent.getType().equals(TYPE)){
+			damage -= 10;
+		}
+
+		return damage;
+	}
+
+	public int blast(Pokemon opponent, String def){
+		int damage = 0;
+
+		if (def.equals("shell")){
+			damage += 10;
+		}
+
+		if (def.equals("shield")){
+			damage  += 10;
+		}
+
+		if (def.equals("simple")){
+			damage  += 25;
+		}
+
+		if(opponent.getType().equals(TYPE)){
+			damage -= 10;
+		}
+
+		return damage;
+	}
+
+	public int charge(Pokemon opponent, String def){
+		int damage = 0;
+
+		if (def.equals("shell")){
+			damage += 5;
+		}
+
+		if (def.equals("shield")){
+			damage  += 5;
+		}
+
+		if (def.equals("simple")){
+			damage  += 0;
+		}
+
+		if(opponent.getType().equals(TYPE)){
+			damage -= 10;
+		}
+
+		return damage;
 	}
 
 }
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -4,9 +4,11 @@ abstract public class Pokemon {
 	private int maxhealth;
 	private String name;
 	private int currenthealth;
-	
+
+
 	// grabs attack for specific pokemon
 	abstract public void attack();
+
 	
 	// subtracts damage from current health
 	public void takeDamage(int damage) {

76798b749d036136de67e14f5a86d5e51908cf94
JoshSpicher <83559457+JoshSpicher@users.noreply.github.com>
2021-05-04 20:37:33 -0400

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,5 +1,5 @@
 
-public class Item {
+public class Item extends Trainer {
 	
 	private String name;
 	private int price;
@@ -8,7 +8,7 @@ public class Item {
 		this.name = name;
 		this.price = price;
 	}
-
+	
 	// returns item name
 	public String getName() {
 		return name;
@@ -19,9 +19,38 @@ public class Item {
 		return price;
 	}
 	
+	public void maxRevive() {
+		if(currentHealth == 0) {
+			currentHealth += 50;
+		}
+		else {
+			System.out.println("Pokemon must have health of 0 to use Max Revive.");
+		}
+		coins - 3;
+	}
 	
-
-
-
-
-}
+	public void potion() {
+		if(currentHealth < 91) {
+			currentHealth += 10;
+		}
+		else if(currentHealth == 0) {
+			System.out.println("Sorry, Pokemon must be revived in order to use Potion.");
+		}
+		else {
+			System.out.println("Pokemon's health is to high to use Potion.");
+		}
+		coins - 1;
+	}
+	
+	public void superPotion() {
+		if(currentHealth < 76) {
+			currentHealth += 25;
+		}
+		else if(currentHealth == 0) {
+			System.out.println("Sorry, Pokemon must be revived in order to use Super Potion.");
+		}
+		else {
+			System.out.println("Pokemon's health is to high to use Super Potion.");
+		}
+		coins - 2;
+	}

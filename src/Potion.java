416609709ff1735e7167a56315e8d955782d8e09
JoshSpicher <83559457+JoshSpicher@users.noreply.github.com>
2021-05-05 13:44:57 -0400

    Update Potion.java

--- src/Potion.java
+++ src/Potion.java
@@ -1,10 +1,17 @@
-
 public class Potion extends Item {
-	
+	/**
+	 * creates a item called potion
+	 * @param name
+	 * @param price
+	 * JoshSpicher
+	 */
 	public Potion(String name, int price) {
 		super(name, price);
 	}
-	
+	/**
+	 * creates a use method for potion that restores 10 health to a pokemon of your choice
+	 * JoshSpicher
+	 */
 	@Override
 	public void use(Pokemon p) {
 		if(p.getCurrentHealth() > 0) {

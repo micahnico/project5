af52981ba723d2caf5088029cdf74b8d528f5919
JoshSpicher <83559457+JoshSpicher@users.noreply.github.com>
2021-05-05 13:48:40 -0400

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,23 +1,43 @@
+
 public abstract class Item {
 	
 	private String name;
 	private int price;
 	
+	/**
+	 * creates a new item and gives it a name and price
+	 * @param name 
+	 * @param price
+	 * JoshSpicher
+	 */
 	public Item(String name, int price) {
 		this.name = name;
 		this.price = price;
 	}
-	
+	/**
+	 * 
+	 * @return the name of the item
+	 * JoshSpicher
+	 * 
+	 */
 	// returns item name
 	public String getName() {
 		return name;
 	}
-
+	/**
+	 * 
+	 * @return the price of the item
+	 * JoshSpicher
+	 */
 	// returns item price
 	public int getPrice() {
 		return price;
 	}
-	
+	/**
+	 * 
+	 * @param p the name of the pokemon you would like to use the item on
+	 * JoshSpicher
+	 */
 	public abstract void use(Pokemon p);
 	
 }

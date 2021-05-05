99965134bec910641b55cac729a6e377f0398bf3
JoshSpicher <83559457+JoshSpicher@users.noreply.github.com>
2021-05-05 13:44:30 -0400

    Update MaxRevive.java

--- src/MaxRevive.java
+++ src/MaxRevive.java
@@ -1,9 +1,18 @@
 public class MaxRevive extends Item {
 	
+	/**
+	 * creates an item called max revive
+	 * @param name
+	 * @param price
+	 * JoshSpicher
+	 */
 	public MaxRevive(String name, int price) {
 		super(name, price);
 	}
-	
+	/**
+	 * creates a use method for max revive that restores a pokemon of your choices health back to max after it has fainted by reaching 0
+	 * JoshSpicher
+	 */
 	@Override
 	public void use(Pokemon p) {
 		if(p.getCurrentHealth() == 0) {

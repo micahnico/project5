3b9aaf2226d3d36cfb5c1a7e39ac040ae2f21e31
JoshSpicher <83559457+JoshSpicher@users.noreply.github.com>
2021-05-04 20:21:51 -0400

    Update Pokemon.java

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -3,28 +3,23 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-	public class Pokemon {
+public class Pokemon {
 
 	private int maxHealth;
 	private String typePokemon;
 	private int currentHealth;
-	private int baseDamage;
-	private int critDamage;
 	private String name;
 
 	//There is an inheritance three of attacks
 	//Lists of attacks of the pokemon
-	//Collections need to be checked on that - Philippe
 	private ArrayList<Attack> attacks;
 	private ArrayList<Defense> defenses;
 
-	public Pokemon(String type, String newName, int maxHealth, int baseDamage, int critDamage) {
+	public Pokemon(String type, String newName, int maxHealth) {
 		this.typePokemon = type;
 		this.name = newName;
 		this.maxHealth = maxHealth;
 		this.currentHealth = maxHealth;
-		this.baseDamage = baseDamage;
-		this.critDamage = critDamage;
 		attacks = new ArrayList<>();
 		defenses = new ArrayList<>();
 	}
@@ -33,7 +28,7 @@ import java.util.Scanner;
 		return this.name;
 	}
 
-		// returns type of Pokemon
+	// returns type of Pokemon
 	public String getTypePokemon() {
 		return typePokemon;
 	}
@@ -48,37 +43,11 @@ import java.util.Scanner;
 		return currentHealth;
 	}
 
-	//I should not need this anymore - Philippe
-	// attack deals base damage to opposing Pokemon
-	public void dealBaseDamage() {
-		baseDamage = ((int) (Math.random() * (20-10))) + 10;
-		if(currentHealth - baseDamage < 0) {
-			currentHealth = 0;
-		}
-		else {
-			currentHealth -= baseDamage;
-		}
-	}
-
-	//I should not need this also - Philippe
-	// attack deals crit damage to opposing Pokemon
-	public void dealCritDamage() {
-		critDamage = ((int) (Math.random() * (40-30))) + 30;
-		if(currentHealth - critDamage < 0) {
-			currentHealth = 0;
-		}
-		else {
-			currentHealth -= critDamage;
-		}
-	}
-	
 	// sets current health to max health
 	public void setToMaxHealth() {
 		currentHealth = maxHealth;
 	}
 
-	//I need this simple take damage. I manage the number of health taken elsewhere (in function of type etc ...)
-	// Is that ok? -Philippe
 	public void takeDamage(int damage) {
 		currentHealth -= damage;
 	}
@@ -90,7 +59,10 @@ import java.util.Scanner;
 	public Attack getAttack(int i) {
 		return attacks.get(i);
 	}
-		
-	}
+
+}
+
+
+
 
 
